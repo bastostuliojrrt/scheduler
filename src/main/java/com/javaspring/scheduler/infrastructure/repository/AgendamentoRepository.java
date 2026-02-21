5db4a87d97cf4aa723bd5099c2ef265e80ff356f
bastostuliojrrt <bastostuliojrrt@gmail.com>
2026-02-20 22:51:45 -0300

    Feat: criação da classe AgendamentoService e do método salvarAgendamento.

--- src/main/java/com/javaspring/scheduler/infrastructure/repository/AgendamentoRepository.java
+++ src/main/java/com/javaspring/scheduler/infrastructure/repository/AgendamentoRepository.java
@@ -3,5 +3,10 @@ package com.javaspring.scheduler.infrastructure.repository;
 import com.javaspring.scheduler.infrastructure.entity.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface AgendamentoRepository  extends JpaRepository<Agendamento, Long> {
+
+    Agendamento findByServicoAndDataHoraAgendamentoBetween(String servico, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFinal);
+
 }
