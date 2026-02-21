9c14d76c4f4c2559469080531ceaa9b173e79fde
bastostuliojrrt <bastostuliojrrt@gmail.com>
2026-02-20 23:41:10 -0300

    Feat: criação dos métodos de deleção, get de agendamentos por dia, alteração de agendamento por cliente.

--- src/main/java/com/javaspring/scheduler/services/AgendamentoService.java
+++ src/main/java/com/javaspring/scheduler/services/AgendamentoService.java
@@ -4,6 +4,8 @@ import com.javaspring.scheduler.infrastructure.entity.Agendamento;
 import com.javaspring.scheduler.infrastructure.repository.AgendamentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -12,8 +14,10 @@ import java.util.Objects;
 
 public class AgendamentoService {
 
+    // Instância do repository
     private final AgendamentoRepository agendamentoRepository;
 
+    // Método para salvar o agendamento verificando se não existe agenda já marcado para aquela dataHora
     public Agendamento salvarAgendamento(Agendamento agendamento){
 
         LocalDateTime horaAgendamento = agendamento.getDataHoraAgendamento();
@@ -28,7 +32,37 @@ public class AgendamentoService {
 
        return agendamentoRepository.save(agendamento);
 
+    }
+
+    // Método para deleção de agendamentos
+    public void deletarAgendamento(LocalDateTime dataHoraAgendamento, String cliente){
+
+        agendamentoRepository.deleteByDataHoraAgendamentoAndCliente(dataHoraAgendamento, cliente);
 
     }
+
+    // Método para buscar os agendamentos do dia
+    public Agendamento buscarAgendamentosDoDia(LocalDate data){
+        LocalDateTime primeiraHoraDia = data.atStartOfDay();
+        LocalDateTime horaFinalDia = data.atTime(23, 59, 59);
+
+        return agendamentoRepository.findByDataHoraAgendamentoBetween(primeiraHoraDia, horaFinalDia);
+    }
+
+    // Alteração de agendamento por cliente
+    public Agendamento alterarAgendamento(Agendamento agendamento, LocalDateTime dataHoraAgendamento, String cliente){
+
+        // Procurar o agendamento e guarda-lo em "agenda"
+        Agendamento  agenda = agendamentoRepository.findByDataHoraAgendamentoAndCliente(dataHoraAgendamento, cliente);
+
+        if(Objects.isNull(agenda)){
+            throw new RuntimeException("Horário não está preenchido.");
+        }
+
+        agendamento.setId(agenda.getId());
+        return agendamentoRepository.save(agendamento);
+
+    }
+
 
 }
