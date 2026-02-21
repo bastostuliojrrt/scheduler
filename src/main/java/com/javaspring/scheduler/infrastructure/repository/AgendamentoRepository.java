1f9725802831e25144559d67fc13e252b40ce206
bastostuliojrrt <bastostuliojrrt@gmail.com>
2026-02-20 23:37:49 -0300

    Feat: criação de query methods para delete, get de agendamentos por dia e alteração do agendamento.

--- src/main/java/com/javaspring/scheduler/infrastructure/repository/AgendamentoRepository.java
+++ src/main/java/com/javaspring/scheduler/infrastructure/repository/AgendamentoRepository.java
@@ -2,11 +2,22 @@ package com.javaspring.scheduler.infrastructure.repository;
 
 import com.javaspring.scheduler.infrastructure.entity.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 public interface AgendamentoRepository  extends JpaRepository<Agendamento, Long> {
 
     Agendamento findByServicoAndDataHoraAgendamentoBetween(String servico, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFinal);
+
+    // DELETE
+    @Transactional
+    void deleteByDataHoraAgendamentoAndCliente(LocalDateTime dataHoraAgendamento, String cliente);
+
+    //GET por dia
+    Agendamento findByDataHoraAgendamentoBetween(LocalDateTime dataHoraInicial, LocalDateTime dataHoraFinal);
+
+    // Alteração do agendamento
+    Agendamento findByDataHoraAgendamentoAndCliente(LocalDateTime dataHoraAgendamento, String cliente);
 
 }
